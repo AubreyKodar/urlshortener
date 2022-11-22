26cdda03e8d43bc6b94f4d480fa4ffba3be015bc
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2022-11-22 23:11:58 +0200

    Added url generate page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,87 +1,54 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+  <v-container fill-height>
+    <v-row justify="center" align="center">
+      <v-col md="4" cols="12" class="text-center">
+        <v-form ref="fromGenerate" lazy-validation>
+          <h3 class="mb-8">URL Shortener</h3>
+          <v-text-field
+            label="URL"
+            class="mb-8"
+            v-model="url"
+            validate-on-blur
+            :rules="validationRules.url"
+          ></v-text-field>
+          <v-btn
+            class="primary"
+            left
+            @click="onGenerateUrl"
           >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+            <v-icon small>mdi-link-variant</v-icon>
+            Generate
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  head() {
+    return {
+      title: 'Home',
+    }
+  },
+  data () {
+    return {
+      url: null,
+      validationRules: {
+        url: [
+          v => !!v || 'Url field cannot be empty',
+        ],
+      }
+    }
+  },
+  methods: {
+    onGenerateUrl() {
+      if (this.$refs.fromGenerate.validate()) {
+
+      }
+    },
+  }
 }
 </script>
