fc34bb20b62c63e7d9132e9afe86edc1ddbd4fdf
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2022-11-25 06:19:00 +0200

    Added server and mongoose setup

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,32 @@
 const express = require('express');
-const http = require('http');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dbConfig = require('./config/database');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
-const server = http.createServer(app)
+const apiRoutes = require('./routes/api');
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api', apiRoutes);
+
+const SERVER_PORT = 3000;
+
+mongoose
+    .connect(dbConfig.url(), {
+        useNewUrlParser: true,
+        user: dbConfig.user,
+        pass: dbConfig.pwd,
+    })
+    .then(() => {
+        app.listen(SERVER_PORT);
+        console.log(`listening on port ${ SERVER_PORT }`)
+    })
+    .catch(err => {
+        console.log('connection failed');
+        console.log(err);
+    });
 
