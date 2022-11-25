774b70a7759102155b31c885ebf862a4e6afa591
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2022-11-25 11:00:49 +0200

    Display call back end to generate short url and display

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,7 +2,7 @@
   <v-container fill-height>
     <v-row justify="center" align="center">
       <v-col md="4" cols="12" class="text-center">
-        <v-form ref="fromGenerate" lazy-validation>
+        <v-form ref="fromGenerate" lazy-validation :disabled="loading">
           <h3 class="mb-8">URL Shortener</h3>
           <v-text-field
             label="URL"
@@ -10,9 +10,12 @@
             v-model="url"
             validate-on-blur
             :rules="validationRules.url"
+            clearable
+            @click:clear="onClearUrlField"
           ></v-text-field>
           <v-btn
             class="primary"
+            :loading="loading"
             left
             @click="onGenerateUrl"
           >
@@ -20,6 +23,30 @@
             Generate
           </v-btn>
         </v-form>
+        <v-row class="mt-12 grey white--text">
+          <v-col cols="12">
+            <div style="width: 80%; float: left">
+              <pre>{{ shortUrl }}</pre>
+            </div>
+            <div style="width: 20%; float: left">
+              <v-btn
+                small
+                class="white"
+                :rounded="false"
+                @click="copyToClipBoard"
+              >
+                <v-icon>mdi-content-copy</v-icon>
+              </v-btn>
+              <v-snackbar
+                absolute
+                tile
+                v-model="snackBar.show"
+              >
+                {{ snackBar.text }}
+              </v-snackbar>
+            </div>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
@@ -36,19 +63,49 @@ export default {
   data () {
     return {
       url: null,
+      shortUrl: null,
+      loading: false,
       validationRules: {
         url: [
           v => !!v || 'Url field cannot be empty',
         ],
+      },
+      snackBar: {
+        show: false,
+        text: 'Url copied',
       }
     }
   },
   methods: {
-    onGenerateUrl() {
+    async onGenerateUrl() {
       if (this.$refs.fromGenerate.validate()) {
+        this.loading = true;
 
+        try {
+          const shortUrl = await this.$api.shortUrl.createShortUrl(this.url);
+          this.shortUrl = shortUrl.url;
+        } catch (e) {
+          console.log(e);
+        } finally {
+          this.loading = false;
+        }
       }
     },
+    copyToClipBoard() {
+      navigator.clipboard.writeText(this.shortUrl);
+      this.snackBar.show = true;
+
+      setTimeout(() => {
+        this.snackBar.show = false;
+      }, 1000)
+    },
+    onClearUrlField() {
+      this.shortUrl = null;
+    }
   }
 }
 </script>
+
+<style>
+
+</style>
