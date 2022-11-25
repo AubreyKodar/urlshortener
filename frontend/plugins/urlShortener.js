060e01139bc9cd5f84297962605b597c47f2557f
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2022-11-25 11:18:41 +0200

    refactor api name

--- frontend/plugins/urlShortener.js
+++ frontend/plugins/urlShortener.js
@@ -1,6 +1,6 @@
 export default axios => ({
   createShortUrl: longUrl => axios.$post('/api/encode', { url: longUrl }),
   getLongUrl: shortUrl => axios.$post('/api/decode', { url: shortUrl }),
-  getShortUrlList: () => axios.$get('/api/list'),
+  getUrlList: () => axios.$get('/api/list'),
   getStatistics: path => axios.$get(`/api/statistic/${ path }`),
 });
