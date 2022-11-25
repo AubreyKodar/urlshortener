bc8b8af5ae711c819e81efb6f1a8d1102c754b42
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2022-11-25 12:01:03 +0200

    Added search logic

--- frontend/pages/list.vue
+++ frontend/pages/list.vue
@@ -1,13 +1,86 @@
 <template>
   <v-container>
-    <v-data-table>
+    <h3 class="text-center mb-4">Url List</h3>
+    <v-row class="mb-6">
+      <v-col cols="12" md="4" offset-md="8">
+        <v-text-field
+          label="Search"
+          v-model="search"
+          prepend-icon="mdi-magnify"
+          @input="onSearch"
+          clearable
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-data-table
+      :headers="headers"
+      :items="tableItems"
+      :loading="loading"
+    >
 
     </v-data-table>
   </v-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
+const MIN_CHARS_TO_SEARCH = 2;
+
 export default {
-  name: "list"
+  name: "list",
+  data() {
+    return {
+      search: null,
+      searchResults: [],
+      headers: [
+        {
+          text: 'Long Url',
+          align: 'left',
+          sortable: false,
+          value: 'longUrl',
+        },
+        {
+          text: 'Short Url',
+          align: 'left',
+          sortable: false,
+          value: 'shortUrl',
+        },
+        {
+          text: 'Clicks',
+          align: 'left',
+          sortable: false,
+          value: 'clicks',
+        },
+        {
+          text: 'Last Used',
+          align: 'left',
+          sortable: false,
+          value: 'lastAccess',
+        },
+      ],
+    }
+  },
+  computed: {
+    ...mapGetters({
+      loading: 'url/getIsReady',
+      urls: 'url/getUrls'
+    }),
+    tableItems () {
+      return (this.search && this.search.length > MIN_CHARS_TO_SEARCH) ? this.searchResults : this.urls;
+    }
+  },
+  mounted() {
+    this.$store.dispatch('url/fetchUrls');
+  },
+  methods: {
+    onSearch () {
+      if (this.search && this.search.length > MIN_CHARS_TO_SEARCH) {
+        this.searchResults = this.urls.filter(url => {
+          return url.longUrl.includes(this.search);
+        });
+      }
+    }
+  }
 }
 </script>
--- frontend/plugins/urlShortener.js
+++ frontend/plugins/urlShortener.js
@@ -1,6 +1,5 @@
 export default axios => ({
   createShortUrl: longUrl => axios.$post('/api/encode', { url: longUrl }),
-  getLongUrl: shortUrl => axios.$post('/api/decode', { url: shortUrl }),
   getUrlList: () => axios.$get('/api/list'),
   getStatistics: path => axios.$get(`/api/statistic/${ path }`),
 });
