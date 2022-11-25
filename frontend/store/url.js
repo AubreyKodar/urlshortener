6ae40979bf8da6d2d02ad433e32ce7965871fea7
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2022-11-25 11:18:17 +0200

    Added url store

---
+++ frontend/store/url.js
@@ -0,0 +1,36 @@
+export const state = () => ({
+  isReady: true,
+  urls: [],
+});
+
+export const mutations = {
+  SET_IS_READY(state, isReady) {
+    state.isReady = isReady;
+  },
+  SET_URLS(state, urls) {
+    state.urls = urls;
+  }
+};
+
+export const actions = {
+  async fetchUrls({ commit }) {
+    commit('SET_IS_READY', false);
+
+    try {
+      const urls = await this.$api.shortUrl.getUrlList();
+      commit('SET_URLS', urls);
+    } finally {
+      commit('SET_IS_READY', true);
+    }
+
+  }
+};
+
+export const getters = {
+  getIsReady(state) {
+    return !state.isReady;
+  },
+  getUrls(state) {
+    return state.urls;
+  }
+};
